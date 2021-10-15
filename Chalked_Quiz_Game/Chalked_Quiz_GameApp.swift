3751a0e23dda930d0b5987101835b9b59375abc8
Amy Jia Ying Tan <atan59@my.bcit.ca>
2021-10-15 15:14:58 -0700

    Initial Commit

---
+++ Chalked_Quiz_Game/Chalked_Quiz_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chalked_Quiz_GameApp.swift
+//  Chalked_Quiz_Game
+//
+//  Created by Amy Tan on 2021-10-15.
+//
+
+import SwiftUI
+
+@main
+struct Chalked_Quiz_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chalked_Quiz_Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Chalked_Quiz_Game
+//
+//  Created by Amy Tan on 2021-10-15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
