903b8f5dd67a807f3abd76eebe7f826a4cad48de
Amy Jia Ying Tan <atan59@my.bcit.ca>
2021-10-15 15:16:26 -0700

    Initial Commit

--- Chalked_Quiz_Game/ContentView.swift
+++ Chalked_Quiz_Game/ContentView.swift
@@ -1,16 +1,99 @@
 //
 //  ContentView.swift
-//  Chalked_Quiz_Game
+//  TestQuiz
 //
-//  Created by Amy Tan on 2021-10-15.
+//  Created by Amy Tan on 2021-10-08.
 //
 
 import SwiftUI
 
-struct ContentView: View {
+let timer = Timer
+    .publish(every: 1, on: .main, in: .common)
+    .autoconnect()
+
+struct Clock: View {
+    var counter: Int
+    var countTo: Int
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(counterToMinutes())
+                .font(.custom("Avenir Next", size: 60))
+                .fontWeight(.black)
+        }
+    }
+    
+    func counterToMinutes() -> String {
+        let currentTime = countTo - counter
+        let seconds = currentTime % 60
+        let minutes = Int(currentTime / 60)
+        
+        return "\(minutes):\(seconds < 10 ? "0" : "")\(seconds)"
+    }
+    
+}
+
+struct ProgressTrack: View {
+    var body: some View {
+        Circle()
+            .fill(Color.clear)
+            .frame(width: 250, height: 250)
+            .overlay(
+                Circle().stroke(Color.black, lineWidth: 15)
+        )
+    }
+}
+
+struct ProgressBar: View {
+    var counter: Int
+    var countTo: Int
+    
+    var body: some View {
+        Circle()
+            .fill(Color.clear)
+            .frame(width: 250, height: 250)
+            .overlay(
+                Circle().trim(from:0, to: progress())
+                    .stroke(
+                        style: StrokeStyle(
+                            lineWidth: 15,
+                            lineCap: .round,
+                            lineJoin:.round
+                        )
+                )
+                    .foregroundColor(
+                        (completed() ? Color.green : Color.orange)
+                ).animation(
+                    .easeInOut(duration: 0.2)
+                )
+        )
+    }
+    
+    func completed() -> Bool {
+        return progress() == 1
+    }
+    
+    func progress() -> CGFloat {
+        return (CGFloat(counter) / CGFloat(countTo))
+    }
+}
+
+struct ContentView: View {
+    @State var counter: Int = 0
+    var countTo: Int = 120
+    
+    var body: some View {
+        VStack{
+            ZStack{
+                ProgressTrack()
+                ProgressBar(counter: counter, countTo: countTo)
+                Clock(counter: counter, countTo: countTo)
+            }
+        }.onReceive(timer) { time in
+            if (self.counter < self.countTo) {
+                self.counter += 1
+            }
+        }
     }
 }
 
@@ -19,3 +102,32 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+//struct ContentView: View {
+//    var body: some View {
+//        VStack {
+//            Text("<Question Placeholder>").padding(.bottom)
+//            Group {
+//                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+//                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+//                }
+//                Button(action: {}) {
+//                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+//                }
+//                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+//                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+//                }
+//                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+//                    /*@START_MENU_TOKEN@*/Text("Button")/*@END_MENU_TOKEN@*/
+//                }
+//            }.padding(.bottom)
+//
+//        }.padding()
+//    }
+//}
+//
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
