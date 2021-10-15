df584f337d546c34325671ea5f72c8ce278dd076
Amy Jia Ying Tan <atan59@my.bcit.ca>
2021-10-15 14:59:43 -0700

    First Commit

---
+++ TestQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestQuizApp.swift
+//  TestQuiz
+//
+//  Created by Amy Tan on 2021-10-08.
+//
+
+import SwiftUI
+
+@main
+struct TestQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
